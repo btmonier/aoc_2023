380f2f05118d37f775b9bbd773e3f1fe1dc5be09
Brandon <brandon.monier@gmail.com>
2023-12-07 12:55:53 -0500

    Add Day 2 - Part 2 solution

--- src/R/02.R
+++ src/R/02.R
@@ -11,7 +11,7 @@ testInputP01 <- list(
     "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
 )
 
-testInputP01 <- list(
+testInputP02 <- list(
     "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
     "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
     "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
@@ -85,12 +85,62 @@ inputPath |>
 ## Part 2 ----
 
 ### Solver
-partTwoSolver <- function(l) {
+maxCubes <- list(
+    "red"   = 12,
+    "green" = 13,
+    "blue"  = 14
+)
 
+partTwoSolver <- function(l) {
+    m <- l |>
+        lapply(\(it) {
+            currGame <- it |> gsub("Game |:.*$", "", x = _) |> as.numeric()
+
+            gameRes <- it |>
+                gsub("Game .*: ", "", x = _) |>
+                strsplit("; ") |>
+                unlist() |>
+                strsplit(", ")
+
+            maxRGBs <- list(
+                "red"   = 0,
+                "green" = 0,
+                "blue"  = 0
+            )
+
+            gameRes |> lapply(\(ev) {
+                for (mc in maxCubes |> names()) {
+                    cubeValue <- ev[grepl(mc, ev)] |>
+                        gsub(" .*$", "", x = _) |>
+                        as.numeric() |>
+                        sum()
+
+                    if (cubeValue > maxRGBs[[mc]]) {
+                        maxRGBs[[mc]] <<- cubeValue
+                    }
+                }
+            })
+
+            return(maxRGBs |> unlist() |> prod())
+
+        })
+
+    return(m |> unlist() |>  sum())
 }
 
 
+### Test
+testthat::expect_equal(
+    testInputP02 |> partTwoSolver(),
+    2286
+)
 
 
+### Actual
+cat("=== PART TWO ===\n")
+inputPath |>
+    readLines() |>
+    partTwoSolver() |>
+    print()
 
 
